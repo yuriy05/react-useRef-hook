7ebdeef04ed6f0f068e4aacff45710082a1740c9
yuriy05 <dilayyura@gmail.com>
2023-12-18 20:20:19 +0200

    React practice | useRef hook |

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,57 @@
-import React from "react";
+import {useRef} from "react";
+import Page from "./component/page";
+import Grid from "./component/grid";
 
 function App() {
-  return <div>Hello world</div>;
+
+  const firstPicture = useRef(null);
+  const secondPicture = useRef(null);
+  const thirdPicture = useRef(null);
+
+  function handleScrollBy(ref) {
+    if (ref && ref.current) {
+      const offsetTop = ref.current.offsetTop;
+      window.scrollBy({
+        top: offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }
+
+  return(
+    <Page>
+      <Grid>
+        <button onClick={() => handleScrollBy(firstPicture)}>First image</button>
+        <button onClick={() => handleScrollBy(secondPicture)}>Second image</button>
+        <button onClick={() => handleScrollBy(thirdPicture)}>Third image</button>
+      </Grid>
+      <div>
+        <ul style={{ display: "grid", gap: "500px", marginBottom: "500px"}}>
+          <li>
+            <img
+              src="https://picsum.photos/200/300"
+              alt="pic"
+              ref={firstPicture}
+            />
+          </li>
+          <li>
+            <img
+              src="https://picsum.photos/200/300"
+              alt="pic"
+              ref={secondPicture}
+            />
+          </li>
+          <li>
+            <img
+              src="https://picsum.photos/200/300"
+              alt="pic"
+              ref={thirdPicture}
+            />
+          </li>
+        </ul>
+      </div>
+    </Page>
+  )
 }
 
 export default App;
